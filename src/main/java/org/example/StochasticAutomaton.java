8e4751c40909860a55f1a657dbf8ff3561b30a69
abulashvili <abulashvili@krista.ru>
2023-02-17 10:59:07 +0300

    feat: реализовать моделирование

--- src/main/java/org/example/StochasticAutomaton.java
+++ src/main/java/org/example/StochasticAutomaton.java
@@ -1,9 +1,16 @@
 package org.example;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
+import java.util.stream.Collectors;
 
 public class StochasticAutomaton {
+    private final List<List<Double>> empiricalStochasticStates;
+
+    private final List<List<Double>> theoreticalStochasticStates;
+
     public static void main(String[] args) {
 
         double[] stochasticState = {1./3, 1./3, 1./3};
@@ -14,13 +21,73 @@ public class StochasticAutomaton {
                 {2./3, 1./3, 0},
         };
 
-        System.out.println(Arrays.toString(multiplyMatrixByVector(jumpMatrix, stochasticState)));
+        StochasticAutomaton stochasticAutomaton = new StochasticAutomaton(jumpMatrix, stochasticState);
+//        System.out.println(stochasticAutomaton.getEmpiricalStochasticStates());
+//        System.out.println("----------------------------");
+//        System.out.println(stochasticAutomaton.getTheoreticalStochasticStates());
     }
 
-    private int getNewStateFromStochasticState(double[] stochasticState) {
-        double prob = ThreadLocalRandom.current().nextDouble(0, 1);
+    public StochasticAutomaton(double[][] jumpMatrix, double[] firstStochasticState) {
+        if (jumpMatrix.length == 0) throw new IllegalArgumentException();
+        if (jumpMatrix.length != firstStochasticState.length) throw new IllegalArgumentException();
+        if (jumpMatrix[0].length != jumpMatrix.length) throw new IllegalArgumentException();
 
-        System.out.println(prob);
+        empiricalStochasticStates = new ArrayList<>();
+        theoreticalStochasticStates = new ArrayList<>();
+
+        modeling(jumpMatrix, firstStochasticState);
+    }
+
+    public List<List<Double>> getEmpiricalStochasticStates() {
+        return empiricalStochasticStates;
+    }
+
+    public List<List<Double>> getTheoreticalStochasticStates() {
+        return theoreticalStochasticStates;
+    }
+
+    private void modeling(double[][] jumpMatrix, double[] firstStochasticState) {
+        int currentState = getNewState(firstStochasticState);
+
+        double[] curTheoreticalStochasticState = firstStochasticState.clone(), curEmpiricalStochasticState;
+
+        int[] stateJumpNumbers = new int[jumpMatrix.length];
+
+        int step = 1;
+
+        do {
+            ++stateJumpNumbers[currentState];
+
+            curEmpiricalStochasticState = getNewEmpiricalStochasticState(stateJumpNumbers, step);
+
+            empiricalStochasticStates.add(Arrays.stream(curEmpiricalStochasticState).boxed().collect(Collectors.toList()));
+            theoreticalStochasticStates.add(Arrays.stream(curTheoreticalStochasticState).boxed().collect(Collectors.toList()));
+
+            currentState = getNewState(jumpMatrix[currentState]);
+
+            curTheoreticalStochasticState = getNewTheoreticalStochasticState(jumpMatrix, curTheoreticalStochasticState);
+
+            ++step;
+        } while (!isStochasticStatesEqual(
+                empiricalStochasticStates.get(empiricalStochasticStates.size() - 1),
+                theoreticalStochasticStates.get(theoreticalStochasticStates.size() - 1)
+        ));
+    }
+
+    private boolean isStochasticStatesEqual(List<Double> stochasticState1, List<Double> stochasticState2) {
+        System.out.println("Empirical: " + stochasticState1 + ", theoretical: " + stochasticState2);
+
+        for (int index = 0; index < stochasticState1.size(); ++index) {
+            if (Math.abs(stochasticState1.get(index) - stochasticState2.get(index)) > 0.01) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private int getNewState(double[] stochasticState) {
+        double prob = ThreadLocalRandom.current().nextDouble(0, 1);
 
         double intervalBound = .0;
 
@@ -35,12 +102,18 @@ public class StochasticAutomaton {
         return index;
     }
 
-    private static double[] multiplyMatrixByVector(double[][] matrix, double[] vector) {
-        double[] resultVector = new double[vector.length];
+    private double[] getNewEmpiricalStochasticState(int[] stateJumpNumbers, int currentStep) {
+        return Arrays.stream(stateJumpNumbers)
+                .mapToDouble(stateJumpNumber -> (double) stateJumpNumber / currentStep)
+                .toArray();
+    }
 
-        for (int index1 = 0; index1 < matrix.length; ++ index1) {
-            for (int index2 = 0; index2 < matrix.length; ++ index2) {
-                resultVector[index1] += matrix[index2][index1] * vector[index2];
+    private double[] getNewTheoreticalStochasticState(double[][] jumpMatrix, double[] curTheoreticalStochasticState) {
+        double[] resultVector = new double[curTheoreticalStochasticState.length];
+
+        for (int index1 = 0; index1 < jumpMatrix.length; ++ index1) {
+            for (int index2 = 0; index2 < jumpMatrix.length; ++ index2) {
+                resultVector[index1] += jumpMatrix[index2][index1] * curTheoreticalStochasticState[index2];
             }
         }
 
